1334ee1ee40e32e22b146af7e779ad6252454d9e
gvc14 <32004406+gvc14@users.noreply.github.com>
2019-01-06 23:36:46 +0530

    Update AlbumArtWallpaper.py

--- AlbumArtWallpaper.py
+++ AlbumArtWallpaper.py
@@ -1,25 +1,30 @@
-import sys
 import spotipy
 import spotipy.util as util
-import os
 import time
-
-scope = 'user-library-read'
-interval = 5  #Wallpaper change frequency
-
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    print ("Username: " % (sys.argv[0],))
-    sys.exit()
-
-token = util.prompt_for_user_token(username, scope)
-
-if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-for item in results['items']:   			
-    link = item['track']['album']['images'][0]['url']	
-    os.system("gsettings set org.gnome.desktop.background picture-uri " + link)
-    time.sleep(interval)
-
+import os
+interval = 1
+scope = 'user-read-currently-playing'
+username = 'Your Username'
+def tokenGen():
+        token = util.prompt_for_user_token(username,scope,client_id = 'Your_client_id',client_secret='Your_client_secret',
+                                           redirect_uri='http://localhost:8888/callback/')
+        sp = spotipy.Spotify(auth=token)
+        return sp
+sp = tokenGen()
+sp.trace = True
+print(sp.currently_playing())
+while True:
+    user = sp.currently_playing()
+    temp = user
+    try:
+     time.sleep(5)
+     user = sp.currently_playing()
+     if temp == user:
+         continue
+     else:
+         link = user['item']['album']['images'][0]['url']
+         os.system("gsettings set org.gnome.desktop.background picture-uri " + link)
+         time.sleep(5)
+    except spotipy.client.SpotifyException:
+     sp = tokenGen()
+     continue
